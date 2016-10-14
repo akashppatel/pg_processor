76ee5fe1005dda452269ff644e14c0c2cabaf94f
akashppatel <akashpatel.it@gmail.com>
2016-10-14 19:39:50 +0530

    User form connecting to endpoint. to avoid cross-origin start ur index.html via server at localhost:8080

---
+++ app.js
@@ -0,0 +1,23 @@
+angular.module('app', [])
+
+    .constant('apiUriPrefix', '/')
+
+    .controller('mainCtrl', function ($http, $interval, apiUriPrefix) {
+
+        var self = this;
+
+        self.user = {name : null}
+
+        self.purchase = function (user) {
+                    console.log('purchasing ...... ' + user.name);
+
+                    $http.post(apiUriPrefix + 'purchase', user).then(
+                        function (response) {
+                            console.log('In app.js -> purchasing : ' + response);
+                            self.pgResponse = response.data;
+                        },
+                        function (err) {
+                            console.log('In app.js -> error while purchasing : ' + err);
+                        });
+                };
+    });
